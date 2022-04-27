aa2e6494af5e3ae149e4bd70f10498bc71906843
ricky80l <98959778+ricky80l@users.noreply.github.com>
2022-04-27 17:00:59 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,90 @@
+// CHANGE PICTURE MOBILE/DESKTOP
+
+$(window).resize(function() {
+    $(document).ready(image_change($(window).width()));
+});
+
+function image_change(width) {
+    var finestra = width;
+    console.log(finestra);
+    if (finestra < 1064) {
+        $(".image").attr("src", "images/hero-mobile.jpg");
+    } else {
+        $(".image").attr("src", "images/hero-desktop.jpg");
+    }
+};
+
+var include = false;
+var chiocciola = "";
+var ordered = false;
+var space = true;
+
+// CHECK FUNCTIONS
+
+$(".submit").click(function() {
+    $(".submit").addClass("clicked");
+    setTimeout(function() {
+        $(".submit").removeClass("clicked");
+    }, 200);
+    check();
+});
+
+
+function check() {
+    verEmail();
+    orderCheck();
+    verSpace();
+    console.log("Include " + include);
+    console.log("ordered " + ordered);
+    console.log("space " + space);
+    console.log();
+    if (($(".email").val() != "") && (include == true) && (ordered == true) && (space == false)) {
+
+        $(".result").text("Thank you Sir!");
+        reset();
+    } else {
+        $(".result").text("Please provide a valid email");
+        $(".error").addClass("visible");
+        $(".email").addClass("red");
+        reset();
+    }
+}
+
+function verEmail() {
+    chiocciola = $(".email").val();
+    if (chiocciola.includes("@") && chiocciola.includes(".")) {
+        include = true;
+    } else {
+        include = false;
+    }
+}
+
+function orderCheck() {
+    if (chiocciola.indexOf("@") < chiocciola.indexOf(".")) {
+        ordered = true;
+    } else {
+        ordered = false;
+    }
+};
+
+
+function verSpace() {
+    if (chiocciola.includes(" ")) {
+        space = true;
+    } else {
+        space = false;
+    }
+};
+
+
+function reset() {
+    setTimeout(function() {
+        $(".result").text("");
+        $(".error").removeClass("visible");
+        $(".email").removeClass("red");
+    }, 2000);
+    include = false;
+    chiocciola = "";
+    ordered = false;
+    space = true;
+};
